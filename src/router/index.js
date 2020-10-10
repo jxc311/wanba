8bbec793734e50e48e16a652ea864a8ebf70ccee
Qi Xin <321834540@qq.com>
2020-10-10 11:54:07 +0800

    qx-语音房

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,65 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// 去除重复点击时的警告
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import("@/views/Home.vue"),
+    children: [
+      {
+        path:"/",
+        redirect:"/index"
+      },
+      {
+        path:"/index",
+        component:()=>import("@/views/Index.vue")
+      },{
+        path:"/news",
+        component:()=>import("@/views/News.vue")
+      },{
+        path:"/voiceRoom",
+        component:()=>import("@/views/VoiceRoom.vue"),
+        children:[
+          {
+            path:"/voiceRoom",
+            redirect:"/voiceRoom/room"
+          },{
+            path:"room",
+            component:()=>import("@/components/voiceRoom/Room.vue")
+          },{
+            path:"broadcast",
+            component:()=>import("@/components/voiceRoom/Broadcast.vue")
+          },{
+            path:"attention",
+            component:()=>import("@/components/voiceRoom/Attention.vue")
+          },{
+            path:"party",
+            component:()=>import("@/components/voiceRoom/Party.vue")
+          },
+        ]
+      },{
+        path:"/discover",
+        component:()=>import("@/views/Discover.vue")
+      },{
+        path:"/me",
+        component:()=>import("@/views/Me.vue")
+      },
+    ],
+  },{
+    path: '*',
+    component: () => import('@/views/NotFound.vue')
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <router-view></router-view>
+    <van-tabbar v-model="activeKey" :placeholder="true" @change="onChange">
+      <van-tabbar-item to="/index" icon="service-o">玩吧</van-tabbar-item>
+      <van-tabbar-item to="/news" icon="comment-o">消息</van-tabbar-item>
+      <van-tabbar-item to="/voiceRoom" icon="more-o">语音房</van-tabbar-item>
+      <van-tabbar-item to="/discover" icon="browsing-history-o">发现</van-tabbar-item>
+      <van-tabbar-item to="/me" icon="contact">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      activeKey: sessionStorage.getItem("activeKey") | 0,
+    };
+  },
+  methods: {
+    onChange() {
+      sessionStorage.setItem("activeKey", this.activeKey);
+    },
+  },
 };
 </script>
+<style lang="less">
+  .van-tabbar{
+    background-color: #ccc;
+    >.van-tabbar-item--active{
+      background-color: #ccc;
+    }
+  }
+</style>
